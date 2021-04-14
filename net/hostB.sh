27706ecf8f38bfcca8897a862236b0b047492524
nicolovinci <vincinicolo98@gmail.com>
2021-04-14 16:51:29 +0200

    Delete vagrant idea

--- net/hostB.sh
+++
@@ -1,5 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y tcpdump
-ip link set enp0s8 up
-ip addr add 192.168.1.3/24 dev enp0s8
--- net/router1.sh
+++
@@ -1,6 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y tcpdump
-sysctl net.ipv4.ip_forward=1
-ip link set enp0s8 up
-ip addr add 192.168.1.1/24 dev enp0s8
