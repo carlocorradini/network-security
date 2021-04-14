24450fab4df9e5f66d937b750acb91552a4b7a38
Carlo <carlo.ita98@gmail.com>
2021-04-14 21:26:25 +0200

    Working ++

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@
 readonly HOST_IMAGE_NAME=ns_common_host_image
 readonly DOCKER_COMPOSE="docker-compose \
     -f docker-compose.yml \
+    -f common/docker-compose.yml \
     -f hosts/attacker/docker-compose.yml \
     -f hosts/client/docker-compose.yml \
     -f hosts/server/docker-compose.yml "
