f5730068463bbfff3db87bdd16b29e5ec2458145
Carlo <carlo.ita98@gmail.com>
2021-04-15 11:42:38 +0200

    Better structure

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,10 @@
 #
 # VARS
 #
-readonly HOST_IMAGE_NAME=ns_common_host_image
-readonly DOCKER_COMPOSE="docker-compose \
-    -f docker-compose.yml \
-    -f common/docker-compose.yml \
-    -f hosts/attacker/docker-compose.yml \
-    -f hosts/client/docker-compose.yml \
-    -f hosts/server/docker-compose.yml "
+readonly PROJECT_NAME=network_security_lab_01
 
-# Build common host image
-docker build -t $HOST_IMAGE_NAME common
-
-# Build hosts
-eval "$DOCKER_COMPOSE build --build-arg host_image=$HOST_IMAGE_NAME"
-
-# Start hosts
-eval "$DOCKER_COMPOSE up -d"
+# Build & Start hosts
+docker-compose --project-name $PROJECT_NAME up \
+    --force-recreate \
+    --build \
+    --detach
